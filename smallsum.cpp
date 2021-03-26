86d373958576d370bcf76406ab129d8e4bb643e3
Anukrit-netizen <56169595+Anukrit-netizen@users.noreply.github.com>
2021-03-26 17:06:45 +0530

    Create smallsum.cpp

---
+++ smallsum.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+  using namespace std;
+  int findDuplicate(int arr[], int n, int k)
+{
+
+    sort(arr, arr + n);
+
+    int i = 0;
+    while (i < n) {
+        int j, count = 1;
+        for (j = i + 1; j < n && arr[j] == arr[i]; j++)
+            count++;
+  
+        if (count == k)
+            return arr[i];
+  
+        i = j;
+    }
+  
+    return -1;
+}
+  int main()
+  {
+    int n,k;
+    cin>>n>>k;
+    int arr[n];
+    for(int i=0;i<n;i++){
+      cin>>arr[i];
+    }
+    cout<<findDuplicate(arr,n,k)<<endl;
+    return 0;
+  }
